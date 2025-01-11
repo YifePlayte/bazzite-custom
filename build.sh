f542ff9d2b76c215bd1a5fab910c9e4b00ff7e89
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2025-01-11 10:37:52 +0800

    Install Mihomo Party

--- build.sh
+++ build.sh
@@ -10,11 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+# dnf install -y tmux 
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+
+# Install Mihomo Party
+env G_MESSAGES_DEBUG=all RUST_LOG=debug dnf install -y https://github.com/mihomo-party-org/mihomo-party/releases/download/v1.5.13/mihomo-party-linux-1.5.13-x86_64.rpm
